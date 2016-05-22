a2fe7e9ef29a43cf747296180156b2970b309a53
Ali Gholami Rudi <ali@rudi.ir>
2016-05-22 12:37:00 +0430

    uc: fix reading utf-8 characters with more than three bytes
    Reported by Gianni Chiappetta <gianni@runlevel6.org>.

--- uc.c
+++ uc.c
@@ -16,11 +16,11 @@ int uc_len(char *s)
 		return 2;
 	if (~c & 0x10)
 		return 3;
-	if (~c & 0x80)
+	if (~c & 0x08)
 		return 4;
-	if (~c & 0x40)
+	if (~c & 0x04)
 		return 5;
-	if (~c & 0x20)
+	if (~c & 0x02)
 		return 6;
 	return 1;
 }

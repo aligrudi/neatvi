b88455ca8974bd4e3bbd57adcd009f565f6d8be6
Ali Gholami Rudi <ali@rudi.ir>
2024-02-17 01:52:32 +0330

    regex: optimize matching character sequences

--- regex.c
+++ regex.c
@@ -163,6 +163,7 @@ static void ratom_readbrk(struct ratom *ra, char **pat)
 
 static void ratom_read(struct ratom *ra, char **pat)
 {
+	char *s;
 	int len;
 	switch ((unsigned char) **pat) {
 	case '.':
@@ -189,8 +190,15 @@ static void ratom_read(struct ratom *ra, char **pat)
 		(*pat)++;
 	default:
 		ra->ra = RA_CHR;
-		len = uc_len(*pat);
-		ra->s = malloc(8);
+		s = *pat;
+		while ((s == *pat) || !strchr(".^$[(|)*?+{\\", (unsigned char) s[0])) {
+			int l = uc_len(s);
+			if (s != *pat && s[l] != '\0' && strchr("*?+{", (unsigned char) s[l]))
+				break;
+			s += uc_len(s);
+		}
+		len = s - *pat;
+		ra->s = malloc(len + 1);
 		memcpy(ra->s, *pat, len);
 		ra->s[len] = '\0';
 		*pat += len;
@@ -265,16 +273,30 @@ static int brk_match(char *brk, int c, int flg)
 
 static int ratom_match(struct ratom *ra, struct rstate *rs)
 {
-	if (ra->ra == RA_CHR) {
-		int c1 = uc_dec(ra->s);
-		int c2 = uc_dec(rs->s);
-		if (rs->flg & REG_ICASE && c1 < 128 && isupper(c1))
-			c1 = tolower(c1);
-		if (rs->flg & REG_ICASE && c2 < 128 && isupper(c2))
-			c2 = tolower(c2);
-		if (c1 != c2)
+	if (ra->ra == RA_CHR && !(rs->flg & REG_ICASE)) {
+		char *s = ra->s;
+		char *r = rs->s;
+		while (*s && *s == *r)
+			s++, r++;
+		if (*s)
 			return 1;
-		rs->s += uc_len(ra->s);
+		rs->s = r;
+		return 0;
+	}
+	if (ra->ra == RA_CHR) {
+		int pos = 0;
+		while (ra->s[pos]) {
+			int c1 = uc_dec(ra->s + pos);
+			int c2 = uc_dec(rs->s + pos);
+			if (rs->flg & REG_ICASE && c1 < 128 && isupper(c1))
+				c1 = tolower(c1);
+			if (rs->flg & REG_ICASE && c2 < 128 && isupper(c2))
+				c2 = tolower(c2);
+			if (c1 != c2)
+				return 1;
+			pos += uc_len(ra->s + pos);
+		}
+		rs->s += pos;
 		return 0;
 	}
 	if (ra->ra == RA_ANY) {
@@ -591,9 +613,9 @@ static int re_recmatch(struct regex *re, struct rstate *rs, int nsub, regmatch_t
 {
 	int i;
 	rs->pc = 0;
-	for (i = 0; i < LEN(rs->mark); i++)
-		rs->mark[i] = -1;
 	rs->dep = 0;
+	for (i = 0; i < LEN(rs->mark) && i < nsub * 2; i++)
+		rs->mark[i] = -1;
 	if (!re_rec(re, rs)) {
 		for (i = 0; i < nsub; i++) {
 			psub[i].rm_so = i * 2 < LEN(rs->mark) ? rs->mark[i * 2] : -1;

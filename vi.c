5e0d1fe5d0ced166967c0c90f102aa543a75fcdc
Ali Gholami Rudi <ali@rudi.ir>
2026-04-25 18:13:33 +0330

    vi: call term_init before ex_init

--- vi.c
+++ vi.c
@@ -2128,17 +2128,17 @@ int main(int argc, char *argv[])
 	dir_init();
 	syn_init();
 	tag_init();
+	if (xled || xvis)
+		term_init();
 	if (!ex_init(argv + i)) {
-		if (xled || xvis)
-			term_init();
 		if (xvis)
 			vi();
 		else
 			ex();
-		if (xled || xvis)
-			term_done();
 		ex_done();
 	}
+	if (xled || xvis)
+		term_done();
 	free(w_path);
 	reg_done();
 	syn_done();

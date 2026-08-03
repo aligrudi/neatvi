bc8c39c18624911c799f24b7879979e486212d28
Ali Gholami Rudi <ali@rudi.ir>
2026-08-04 02:33:50 +0330

    vi: always use ra for quick access commands

--- vi.c
+++ vi.c
@@ -1708,11 +1708,10 @@ static int vc_quick(int newwin)
 	if (cmd[0])
 		return vc_openpath(cmd, 0, 1, newwin) | VC_WIN;
 	if (isalpha(c) && reg_get(0x80 | c, NULL) != NULL) {
-		char cmd1[8] = {'@', '\\', c};
-		char cmd2[8] = {'r', 'a', '\\', c};
+		char ra[8] = {'r', 'a', '\\', c};
 		if (newwin)
 			vi_wmirror();
-		ex_command(islower(c) ? cmd1 : cmd2);
+		ex_command(ra);
 		return VC_ALL;
 	}
 	if (isalpha(c))
